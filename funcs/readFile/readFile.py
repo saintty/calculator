79a51d6a77509e60bc158751daa6d4a6111d1b78
Artem Uzinger <aauzinger@yandex.ru>
2024-02-29 14:13:32 +0300

    Add create RPN

--- funcs/readFile/readFile.py
+++ funcs/readFile/readFile.py
@@ -2,6 +2,17 @@ import re
 
 VARIABLE_PATTERN = re.compile("^[A-Za-z][A-Za-z0-9]*$")
 OPERATION_PATTERN = re.compile("^[+\-*/^()]$")
+OPERATION_PRIORITY = {"+": 2, "-": 2, "(": 0, ")": 1, "*": 3, "/": 3, "^": 4}
+
+
+class Token:
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        type = "Operand" if self.type == "operand" else "Operation"
+        return "({}: {})".format(type, self.value)
 
 
 def readFile(file_name):
@@ -39,7 +50,7 @@ def handle_line(line, vars):
                 "Error in variable definition: varName contains not allowed symbols")
 
         vars[variable_name] = 0
-        print(make_tokens(line[assign_position + 1:].strip()))
+        ops = calculate(line[assign_position + 1:].strip(), vars)
 
 
 def make_tokens(line):
@@ -77,21 +88,57 @@ def make_tokens(line):
 
         elif re.match(OPERATION_PATTERN, char):
             if (len(var_name)):
-                tokens.append(var_name)
+                tokens.append(Token("operand", var_name))
                 var_name = ""
             elif (len(num)):
-                tokens.append(float(num))
+                tokens.append(Token("operand", float(num)))
                 num = ""
 
-            tokens.append(char)
+            tokens.append(Token("operation", char))
             i += 1
         else:
             raise Exception("Unknown symbol")
 
     if len(var_name):
-        tokens.append(var_name)
+        tokens.append(Token("operand", var_name))
 
     if len(num):
-        tokens.append(float(num))
+        tokens.append(Token("operand", float(num)))
 
     return tokens
+
+
+def calculate(line, vars):
+    tokens = make_tokens(line)
+    stack = []
+    result_expression = []
+
+    for token in tokens:
+        if token.type == "operand":
+            result_expression.append(token)
+        elif token.value == "(":
+            stack.append(token)
+        elif token.value == ")":
+            while len(stack):
+                top = stack.pop(-1)
+
+                if top.value == "(":
+                    break
+
+                result_expression.append(top)
+
+        else:
+            if len(stack) == 0 or OPERATION_PRIORITY[stack[-1].value] < OPERATION_PRIORITY[token.value]:
+                stack.append(token)
+            else:
+                while True:
+                    if len(stack) and OPERATION_PRIORITY[stack[-1].value] >= OPERATION_PRIORITY[token.value]:
+                        result_expression.append(stack.pop(-1))
+                    else:
+                        break
+                stack.append(token)
+
+    while len(stack):
+        result_expression.append(stack.pop(-1))
+
+    return result_expression

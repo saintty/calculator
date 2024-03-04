9672c6f1a72e351e167623b0a0f543cf2e1ce559
Artem Uzinger <aauzinger@yandex.ru>
2024-03-04 20:40:46 +0300

    Add comman line args

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from utils.readFile import readFile
 
+import argparse
+
 if __name__ == '__main__':
-    readFile("expression.txt")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="Input file for the program")
+    args = parser.parse_args()
+    input_file = args.input_file
+    readFile(input_file)
